7f808932c6b47b0c01ff1ed994ae844c565777d1
Eshi <eshi@gmail.com>
2024-11-06 21:38:42 -0600

    initial commit

---
+++ Push/src/test/java/printname/Push/printName.java
@@ -0,0 +1,12 @@
+package printname.Push;
+
+public class printName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Eshita");
+	}
+
+}
